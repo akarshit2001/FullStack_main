b8bf7071caf49f18aa8ef2e9ea7615989e5890fa
akarshit2001 <akarshitsrivastava@gmail.com>
2020-10-30 14:51:01 +0530

    practical15 done

--- js/prac15.js
+++ js/prac15.js
@@ -3,16 +3,20 @@ var sum=0;
 var a= document.getElementById("increase");
 var b= document.getElementById("decrease");
 
+document.getElementById("decrease").disabled=true;
+
+
 a.onclick=function () {
     sum = sum + 1;
     document.getElementById("change").disabled=false;
     document.getElementById("change").innerHTML = sum.toString();
-
+    document.getElementById("decrease").disabled=false;
 
 }
 //document.getElementById("test").innerHTML=sum;
 b.onclick=function(){
 if(sum>0){
+
     document.getElementById("change").disabled=false;
     sum=sum-1;
     document.getElementById("change").innerHTML=sum;
@@ -20,6 +24,7 @@ if(sum>0){
 
 }
 else {
+    document.getElementById("decrease").disabled=true;
     document.getElementById("change").disabled=true;
 }
 }
